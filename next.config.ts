ff0499ea3252eff12cdf2b4d1e7e4366a699d137
FHSohag <fhsohag127@gmail.com>
2026-08-18 22:16:19 +0600

    Include Tesseract worker files in Vercel

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["pdf-parse", "tesseract.js", "mupdf"],
+
+  outputFileTracingIncludes: {
+    "/*": [
+      "./node_modules/tesseract.js/src/worker-script/**/*",
+    ],
+  },
 };
 
 export default nextConfig;
